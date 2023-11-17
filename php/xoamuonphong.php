3bd9142fc1c69a10cd25f008cade66384fca9aea
TuanVux <tuanvukg2703@gmail.com>
2023-11-17 10:27:27 +0700

    hi

--- php/xoamuonphong.php
+++ php/xoamuonphong.php
@@ -10,7 +10,9 @@
             echo 'alert("Xóa thành công!");';
             echo 'window.location.href ="/doan1/index.php";'; // Thay "trang-chu.php" bằng URL trang chủ thực tế của bạn
             echo '</script>';
-    }
+    }else {
+        echo "Lỗi: " . $sql . "<br>" . $conn->error;
+}
 }
     $conn->close();
 ?>
